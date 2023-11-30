d724b04d62e98796379223f49a810aa481e363e6
Kasualix <670067575@qq.com>
2023-11-30 22:44:28 +0800

    Update EarlySetupInitializer.java

--- src/main/java/com/teampotato/enchantedpotato/mixin/EarlySetupInitializer.java
+++ src/main/java/com/teampotato/enchantedpotato/mixin/EarlySetupInitializer.java
@@ -5,15 +5,11 @@ import net.neoforged.fml.loading.FMLLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
-import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
-import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
 import java.io.File;
-import java.util.List;
-import java.util.Set;
 
-public class EarlySetupInitializer implements IMixinConfigPlugin {
+public abstract class EarlySetupInitializer implements IMixinConfigPlugin {
 
     public static final String MOD_ID = "enchantedpotato";
     public static final Logger LOGGER = LogManager.getLogger("EnchantedPotato");
@@ -61,11 +57,4 @@ public class EarlySetupInitializer implements IMixinConfigPlugin {
         if (mixinClassName.contains("poison_of_the_last_breath")) return functionConfig.poisonOfTheLastBreath;
         return true;
     }
-
-    @Override public void onLoad(String s) {}
-    @Override public String getRefMapperConfig() {return null;}
-    @Override public void acceptTargets(Set<String> set, Set<String> set1) {}
-    @Override public List<String> getMixins() {return null;}
-    @Override public void preApply(String s, ClassNode classNode, String s1, IMixinInfo iMixinInfo) {}
-    @Override public void postApply(String s, ClassNode classNode, String s1, IMixinInfo iMixinInfo) {}
 }
